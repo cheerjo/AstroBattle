deb1e324fa70f71890e2b8ff11b508a89a122679
Tom Whitwell <tnwhitwell@gmail.com>
2013-12-04 11:47:28 +0000

    chapter 1 complete, yo!

--- Game Project/main.cpp
+++ Game Project/main.cpp
@@ -1,51 +1,21 @@
-#include <SDL.h>
+#include "Game.h"
 #define SDL_main main
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+Game* g_game = 0;
 
 int main(int argc, char *argv[])
 {
-	//init SDL
+	g_game = new Game();
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	g_game->init("Chapter 1", 100, 100, 640, 480, false);
+
+	while (g_game->running())
 	{
-		//if succeeded, create window
-		g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480,
-		SDL_WINDOW_SHOWN);
-
-	//if window creation succeeded, create renderer
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
 	}
-	else
-	{
-		return 1; //sdl could not init
-	}
-
-	//all succeeded, let's draw!
-
-	//set to black (RGBA)
-
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-	//clear window to black
-
-	SDL_RenderClear(g_pRenderer);
-
-	// show the window
-
-	SDL_RenderPresent(g_pRenderer);
-
-	// set a delay before quitting
-	SDL_Delay(5000);
-
-	//clean up SDL
-
-	SDL_Quit();
+	g_game->clean();
 
 	return 0;
 }
